ad36a44d78f441958aa12835f53c41927e20747a
dclechok <dclechok@gmail.com>
2021-11-08 14:03:42 -0500

    seeding DB, and setting up table schema for reservations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,6 +2,7 @@
  * List handler for reservation resources
  */
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+const knex = require('../db/connection');
 
 const DATE_FORMAT = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/; //valid regex formatting for date
 const TIME_FORMAT = /^(0?[1-9]|1[0-2]):[0-5][0-9]$/;  //valid regex formatting for time
@@ -17,43 +18,50 @@ function bodyHasResultProperty(req, res, next) {
   });
 }
 
-function validateRes(req, res, next){
+function validateReservation(req, res, next){
   const { first_name, last_name, mobile_number, reservation_date, reservation_time, people } = req.body.data;
-  //validate each separate piece of data
+  //validate each separate piece of body data
   if(!people || people === 0 || typeof people != 'number') return next({
     status: 400, message: 'Reservation must include a valid number of people',
   });
-  if(!first_name) return next({
+  else if(!first_name) return next({
     status: 400, message: 'Reservation must include a first_name.'
   });
-  if(!last_name) return next({
+  else if(!last_name) return next({
     status: 400, message: 'Reservation must include a last_name.'
   });
-  if(!mobile_number) return next({
+  else if(!mobile_number) return next({
     status: 400, message: 'Reservation must include a mobile_number.'
   });
-  if(reservation_date == 'undefined' || !reservation_date || reservation_date.length === 0 || !DATE_FORMAT.test(reservation_date)) return next({
+  else if(reservation_date == 'undefined' || !reservation_date || reservation_date.length === 0 || !DATE_FORMAT.test(reservation_date)) return next({
     status: 400, message: 'Reservation must include a valid reservation_date.'
   });
-  if(reservation_time == 'undefined' || !reservation_time || !TIME_FORMAT.test(reservation_time)) return next({
+  else if(reservation_time == 'undefined' || !reservation_time || !TIME_FORMAT.test(reservation_time)) return next({
     status: 400, message: 'Reservation must include a valid reservation_time.'
   });
 
-  next();
+  next(); //validated onto next middleware
 }
 
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  // const queryDate = req.query.date;
+  // if(queryDate){
+  //   return knex
+  //     .from('reservations')
+  //     .select('*')
+  //     .where('reservation_date', 'queryDate');
+  // }else
+  return knex
+  .from('reservations')
+  .select('*')
 }
 
-async function create(req, res, next){
-  // const id = next.Id();
+async function create(req, res){
+  const result = req.body.data;
   res.status(201).json({ data: 'hello' });
 }
 
 module.exports = {
   list,
-  create: [bodyHasResultProperty, validateRes, asyncErrorBoundary(create)],
+  create: [bodyHasResultProperty, validateReservation, asyncErrorBoundary(create)],
 };
