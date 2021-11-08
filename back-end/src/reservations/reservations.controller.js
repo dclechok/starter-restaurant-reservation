6582ac0f2c2ccfa5045e09e6fb2fa33463319a50
dclechok <dclechok@gmail.com>
2021-11-08 09:35:05 -0500

    adding validation middleware for server requests

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,33 @@
 /**
  * List handler for reservation resources
  */
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+
+//validation middleware
+function bodyHasResultProperty(req, res, next) {
+  const result = { data: req.body};
+  console.log(result);
+  if (result) { //if body exists - move to validate body
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A 'result' property is required.",
+  });
+}
+
 async function list(req, res) {
   res.json({
     data: [],
   });
 }
 
+async function create(req, res, next){
+  // const id = next.Id();
+  res.send('hello');
+}
+
 module.exports = {
   list,
+  create: [bodyHasResultProperty, asyncErrorBoundary(create)],
 };
