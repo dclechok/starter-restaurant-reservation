75d07d02b9920826bc70cdea1f480dfe1f2debd6
dclechok <dclechok@gmail.com>
2021-11-08 12:23:36 -0500

    finishing validation for reservation post reqs

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -3,10 +3,11 @@
  */
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
+const DATE_FORMAT = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/; //valid regex formatting for date
+const TIME_FORMAT = /^(0?[1-9]|1[0-2]):[0-5][0-9]$/;  //valid regex formatting for time
 //validation middleware
 function bodyHasResultProperty(req, res, next) {
   const { data } = req.body;
-  console.log(data);
   if (data) { //if body exists - move to validate body
     return next();
   }
@@ -18,14 +19,27 @@ function bodyHasResultProperty(req, res, next) {
 
 function validateRes(req, res, next){
   const { first_name, last_name, mobile_number, reservation_date, reservation_time, people } = req.body.data;
-  if(
-    first_name &&
-    last_name &&
-    mobile_number &&
-    reservation_date &&
-    reservation_time &&
-    people
-  )
+  //validate each separate piece of data
+  if(!people || people === 0 || typeof people != 'number') return next({
+    status: 400, message: 'Reservation must include a valid number of people',
+  });
+  if(!first_name) return next({
+    status: 400, message: 'Reservation must include a first_name.'
+  });
+  if(!last_name) return next({
+    status: 400, message: 'Reservation must include a last_name.'
+  });
+  if(!mobile_number) return next({
+    status: 400, message: 'Reservation must include a mobile_number.'
+  });
+  if(reservation_date == 'undefined' || !reservation_date || reservation_date.length === 0 || !DATE_FORMAT.test(reservation_date)) return next({
+    status: 400, message: 'Reservation must include a valid reservation_date.'
+  });
+  if(reservation_time == 'undefined' || !reservation_time || !TIME_FORMAT.test(reservation_time)) return next({
+    status: 400, message: 'Reservation must include a valid reservation_time.'
+  });
+
+  next();
 }
 
 async function list(req, res) {
@@ -36,10 +50,10 @@ async function list(req, res) {
 
 async function create(req, res, next){
   // const id = next.Id();
-  res.send('hello');
+  res.status(201).json({ data: 'hello' });
 }
 
 module.exports = {
   list,
-  create: [bodyHasResultProperty, asyncErrorBoundary(create)],
+  create: [bodyHasResultProperty, validateRes, asyncErrorBoundary(create)],
 };
