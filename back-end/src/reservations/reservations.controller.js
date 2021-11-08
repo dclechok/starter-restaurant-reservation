6c923a6ef5058b44acd6c9d840602436610fda36
dclechok <dclechok@gmail.com>
2021-11-08 10:14:23 -0500

    some error handling, more middleware

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,15 +5,27 @@ const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
 //validation middleware
 function bodyHasResultProperty(req, res, next) {
-  const result = { data: req.body};
-  console.log(result);
-  if (result) { //if body exists - move to validate body
+  const { data } = req.body;
+  console.log(data);
+  if (data) { //if body exists - move to validate body
     return next();
   }
   next({
     status: 400,
     message: "A 'result' property is required.",
   });
+}
+
+function validateRes(req, res, next){
+  const { first_name, last_name, mobile_number, reservation_date, reservation_time, people } = req.body.data;
+  if(
+    first_name &&
+    last_name &&
+    mobile_number &&
+    reservation_date &&
+    reservation_time &&
+    people
+  )
 }
 
 async function list(req, res) {
