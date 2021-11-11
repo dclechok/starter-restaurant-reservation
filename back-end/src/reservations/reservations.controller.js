77259261791c8995582c441d258f69b28e4a8b88
dclechok <dclechok@gmail.com>
2021-11-10 20:35:25 -0500

    post validation fixed, working post request

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -6,10 +6,11 @@ const knex = require('../db/connection');
 // const reservationService = require('./reservations.service');
 
 const DATE_FORMAT = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/; //valid regex formatting for date
-const TIME_FORMAT = /^(0?[1-9]|1[0-2]):[0-5][0-9]$/;  //valid regex formatting for time
+const TIME_FORMAT = /^(0?[1-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;  //valid regex formatting for time
 //validation middleware
 function bodyHasResultProperty(req, res, next) {
   const { data } = req.body;
+  console.log(data);
   if (data) { //if body exists - move to validate body
     return next();
   }
@@ -21,32 +22,32 @@ function bodyHasResultProperty(req, res, next) {
 
 function validateReservation(req, res, next){
   const { first_name, last_name, mobile_number, reservation_date, reservation_time, people } = req.body.data;
-  //validate each separate piece of body data
-  if(!people || people === 0 || typeof people != 'number') return next({
-    status: 400, message: 'Reservation must include a valid number of people',
-  });
-  else if(!first_name) return next({
+  //validate each separate piece of body 
+  console.log(first_name);
+  if(!first_name) return next({
     status: 400, message: 'Reservation must include a first_name.'
   });
-  else if(!last_name) return next({
+  if(!last_name) return next({
     status: 400, message: 'Reservation must include a last_name.'
   });
-  else if(!mobile_number) return next({
+  if(!mobile_number) return next({
     status: 400, message: 'Reservation must include a mobile_number.'
   });
-  else if(reservation_date == 'undefined' || !reservation_date || reservation_date.length === 0 || !DATE_FORMAT.test(reservation_date)) return next({
+  if(!reservation_date || !DATE_FORMAT.test(reservation_date)) return next({
     status: 400, message: 'Reservation must include a valid reservation_date.'
   });
-  else if(reservation_time == 'undefined' || !reservation_time || !TIME_FORMAT.test(reservation_time)) return next({
+  if(!reservation_time || !TIME_FORMAT.test(reservation_time)) return next({
     status: 400, message: 'Reservation must include a valid reservation_time.'
   });
-
+  if(!people || typeof people !== 'number') return next({
+    status: 400, message: 'Reservation must include a valid number of people',
+  });
+  console.log('worked');
   next(); //validated onto next middleware
 }
 
 async function list(req, res) {
   const queryDate = req.query.date;
-  console.log(queryDate);
   const query = knex
     .from('reservations')
     .select('*') //list all reservations, unless query with a key parameter is provided
