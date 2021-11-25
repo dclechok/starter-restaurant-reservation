d371d0b0f066a3f5a677f84849c9149741571e23
dclechok <dclechok@gmail.com>
2021-11-24 19:59:36 -0500

    building new reservation form, making post request

--- front-end/src/components/ReservationForm.jsx
+++ front-end/src/components/ReservationForm.jsx
@@ -1,20 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react"; //useEffect
 import "./ReservationForm.css";
 import { useHistory } from "react-router";
 
 function ReservationForm() {
-
+  const RESERVATIONS_URL = "http://localhost:5000/reservations";
   let history = useHistory(); //get history of page
+  const defaultReservationData = {
+    first_name: " ",
+    last_name: " ",
+    mobile_number: " ",
+    reservation_date: " ",
+    reservation_time: " ",
+    people: 1, //1 is minimum
+  };
 
-  function handleClick(){ history.goBack(); } 
+  const [reservationData, setReservationData] = useState(
+    defaultReservationData
+  );
 
-  function handleSubmit(e){ 
-      e.preventDefault(); //stop from reloading on submit
-      console.log('hello');
+  function handleClick() {
+    history.goBack(); //cancel form takes us back a page
+  }
+
+  function handleSubmit(e) {
+    //validate fields and make call to API
+    e.preventDefault(); //stop from reloading on submit
+    //useEffect
+    console.log(reservationData);
+    async function saveReservation() {
+      const response = await fetch(
+        RESERVATIONS_URL,
+        {
+          method: "POST",
+          body: JSON.stringify(reservationData),
+        }
+      );
+        console.log(response);
     }
+    saveReservation();
 
-  function handleChange(){
+    setReservationData(defaultReservationData);
+  }
 
+  function handleChange(e) {
+    e.preventDefault(); //stop from reloading on change
+    setReservationData({ [e.target.id]: e.target.value });
   }
 
   return (
@@ -23,34 +53,71 @@ function ReservationForm() {
         <fieldset>
           <legend>Create a New Reservation:</legend>
           <hr />
-          <label>
+          <label htmlFor="first_name">
             First Name:
-            <input type="text" name="first_name" id="first_name" onChange={handleChange} />
+            <input
+              type="text"
+              name="first_name"
+              value={reservationData.first_name}
+              id="first_name"
+              onChange={handleChange}
+            />
           </label>
-          <label>
+          <label htmlFor="last_name">
             Last Name:
-            <input type="text" name="last_name" id="last_name" onChange={handleChange} />
+            <input
+              type="text"
+              name="last_name"
+              value={reservationData.last_name}
+              id="last_name"
+              onChange={handleChange}
+            />
           </label>
-          <label>
+          <label htmlFor="mobile_number">
             Mobile Number:
-            <input name="mobile_number" id="mobile_number" onChange={handleChange} />
+            <input
+              name="mobile_number"
+              value={reservationData.mobile_number}
+              id="mobile_number"
+              onChange={handleChange}
+            />
           </label>
-          <label>
+          <label htmlFor="reservation_date">
             Reservation Date:
-            <input name="reservation_date" id="reservation_date" onChange={handleChange} />
+            <input
+              name="reservation_date"
+              value={reservationData.reservation_date}
+              id="reservation_date"
+              onChange={handleChange}
+            />
           </label>
-          <label>
+          <label htmlFor="reservation_time">
             Reservation Time:
-            <input name="reservation_time" id="reservation_time" onChange={handleChange} />
+            <input
+              name="reservation_time"
+              value={reservationData.reservation_time}
+              id="reservation_time"
+              onChange={handleChange}
+            />
           </label>
-          <label>
+          <label htmlFor="people">
             People:
-            <input name="people" id="people" onChange={handleChange} /> {/*must be at least 1 person*/}
+            <input
+              name="people"
+              id="people"
+              value={reservationData.people}
+              onChange={handleChange}
+            />
+            {/*must be at least 1 person*/}
           </label>
           <br />
           <div className="center-buttons">
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-            <button type="button" onClick={handleClick}>Cancel</button>
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
+            <button type="button" onClick={handleClick}>
+              Cancel
+            </button>
           </div>
         </fieldset>
       </form>
