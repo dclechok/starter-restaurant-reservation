e49bdaf94087dc8b7817b809c0a89a26b0f495ec
dclechok <dclechok@gmail.com>
2021-11-26 18:26:48 -0500

    building out dashboard to show reservations in order by date

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,25 +1,86 @@
 import useQuery from "../utils/useQuery";
+import formatReservationDate from "../utils/format-reservation-date";
+import formatReservationTime from "../utils/format-reservation-time";
+import ErrorAlert from "../layout/ErrorAlert";
 import "./Dashboard.css";
+import { useState, useEffect } from "react";
 
 function Dashboard({ date }) {
   //is there a date query?
   const dateQuery = useQuery();
   const getQueryDate = dateQuery.get("date");
+  const RESERVATIONS_URL = "http://localhost:5000/reservations";
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState("");
+  const [useDate, setUseDate] = useState(getQueryDate ? getQueryDate : date);
 
-  return (
-    <div className="div-width">
-      <h2>Reservations for {getQueryDate ? getQueryDate : date}</h2>{" "}
-      {/*if no query date, go with today's date*/}
-      <hr />
-      {/*list all reservations for whatever date we have*/}
-      
-      <div className="center-buttons">
-        <button type="button">Previous</button>
-        <button type="button">Today</button>
-        <button type="button">Next</button>
+  useEffect(() => {
+    async function getReservationByDate() {
+      try {
+        const response = await fetch(RESERVATIONS_URL + `?date=${useDate}`, {
+          method: "GET",
+        });
+        setReservations(await response.json());
+      } catch (e) {
+        console.error(e, "Failed to fetch get request."); // use ErrorAlert?
+      }
+    }
+    getReservationByDate();
+  }, []);
+  // console.log(reservations.data, 'he?');
+
+  function reservationListItemBuilder(listItem) {
+    const {
+      reservation_id,
+      first_name,
+      last_name,
+      mobile_number,
+      reservation_time,
+      reservation_date,
+      people,
+    } = listItem;
+    formatReservationDate(listItem);
+    formatReservationTime(listItem);
+    return (
+      <div>
+        <span>Reservation ID: {reservation_id}</span>
+        <br />
+        <span>Name: {first_name + " " + last_name}</span>
+        <br />
+        <span>Date of Reservation: {reservation_date}</span>
+        <br />
+        <span>Time of Reservation: {reservation_time}</span>
+        <br />
+        <span>Mobile Number: {mobile_number}</span>
+        <br />
+        <span>Amount of People: {people}</span>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if(reservations.data) {
+    return (
+      <div className="div-width">
+        <h2>Reservations for {useDate}</h2>
+        {/*if no query date, go with today's date*/}
+        <hr />
+        {/*list all reservations for whatever date we have*/}
+        {reservations.data.length === 0 && <p>No reservations for this date found...</p>}
+        <ul className="reservation-list">
+          {reservations.data.map((reservation, index) => (
+            <li className="li-container" key={index}>
+              {reservationListItemBuilder(reservation)}
+            </li>
+          ))}
+        </ul>
+        <div className="center-buttons">
+          <button type="button">Previous</button>
+          <button type="button">Today</button>
+          <button type="button">Next</button>
+        </div>
+      </div>
+    );
+  } else return (<p>"Loading reservations..."</p>); //ErrorAlert?
 }
 
 export default Dashboard;
